f2df6fb073286203f5e0cfab7b61ea46f5b09d7b
quentintruong <quentintruong@users.noreply.github.com>
2018-03-04 19:24:37 -0800

    lab3a

--- P3A/lab3a.c
+++ P3A/lab3a.c
@@ -11,6 +11,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <time.h>
+#include <math.h>
 #include "ext2_fs.h"
 
 
@@ -87,7 +88,7 @@ void summarize_free_block_entries(struct ext2_super_block* super_block, struct e
     __u32 block_size = EXT2_MIN_BLOCK_SIZE << super_block->s_log_block_size;
 
     // Malloc and pread
-    uint8_t *bitmap = malloc(block_size);
+    char* bitmap = malloc(block_size);
     if ((pread(img_fd, bitmap, block_size, bitmap_block * block_size)) == -1) {
         fprintf(stderr, "Error: pread failed\n");
         exit(2);
@@ -95,8 +96,8 @@ void summarize_free_block_entries(struct ext2_super_block* super_block, struct e
 
     // Print out
     for (__u32 i = 0; i < block_count; i++){
-        uint8_t byte = bitmap[i / 8];
-        uint8_t mask = 1 << (i % 8);
+        char byte = bitmap[i / 8];
+        char mask = 1 << (i % 8);
         if ((byte & mask) == 0)
             fprintf(stdout, "%s,%u\n", "BFREE", i + 1);
     }
@@ -116,7 +117,7 @@ void summarize_free_inode_entries(struct ext2_super_block* super_block, struct e
     __u32 block_size = EXT2_MIN_BLOCK_SIZE << super_block->s_log_block_size;
 
     // Malloc and pread
-    uint8_t *bitmap = malloc(block_size);
+    char* bitmap = malloc(block_size);
     if ((pread(img_fd, bitmap, block_size, bitmap_inode * block_size)) == -1) {
         fprintf(stderr, "Error: pread failed\n");
         exit(2);
@@ -124,13 +125,85 @@ void summarize_free_inode_entries(struct ext2_super_block* super_block, struct e
 
     // Print out
     for (__u32 i = 0; i < inode_count; i++){
-        uint8_t byte = bitmap[i / 8];
-        uint8_t mask = 1 << (i % 8);
+        char byte = bitmap[i / 8];
+        char mask = 1 << (i % 8);
         if ((byte & mask) == 0)
             fprintf(stdout, "%s,%u\n", "IFREE", i + 1);
     }
 
     free(bitmap);
+}
+
+/*
+    DIRENT
+    parent inode number (decimal) ... the I-node number of the directory that contains this entry
+    logical byte offset (decimal) of this entry within the directory
+    inode number of the referenced file (decimal)
+    entry length (decimal)
+    name length (decimal)
+    name (string, surrounded by single-quotes). Don't worry about escaping, we promise there will be no single-quotes or commas in any of the file names.
+*/
+void summarize_directory_entry(struct ext2_inode* inode, __u32 inode_num, __u32 block_size){
+    // Useful variable
+    __u32 offset = 0;
+
+    // dir_entry is inside each element of i_block
+    for (int i = 0; i < EXT2_NDIR_BLOCKS; i++) {
+        while (offset < inode->i_size){
+            struct ext2_dir_entry* dir_entry = malloc(sizeof(struct ext2_dir_entry));
+            if (pread(img_fd, dir_entry, sizeof(struct ext2_dir_entry), inode->i_block[i] * block_size + offset) < 0) {
+                fprintf(stderr, "Error: pread failed");
+                exit(2);
+            }
+
+            if (dir_entry->inode == 0){
+                offset += dir_entry->rec_len;
+                dir_entry += dir_entry->rec_len;
+                continue;
+            }
+
+            fprintf(stdout, "%s,%d,%d,%d,%d,%d,'%s'\n",
+                "DIRENT", inode_num,
+                offset, dir_entry->inode,
+                dir_entry->rec_len, dir_entry->name_len,
+                dir_entry->name);
+
+            offset += dir_entry->rec_len;
+            dir_entry += dir_entry->rec_len;
+        }
+    }
+}
+
+/*
+    INDIRECT
+    I-node number of the owning file (decimal)
+    (decimal) level of indirection for the block being scanned ... 1 for single indirect, 2 for double indirect, 3 for triple
+    logical block offset (decimal) represented by the referenced block. If the referenced block is a data block, this is the logical block offset of that block within the file. If the referenced block is a single- or double-indirect block, this is the same as the logical offset of the first data block to which it refers.
+    block number of the (1, 2, 3) indirect block being scanned (decimal) . . . not the highest level block (in the recursive scan), but the lower level block that contains the block reference reported by this entry.
+    block number of the referenced block (decimal)
+*/
+void summarize_indirect_ref(__u32 owner_inode_num, __u32 inode_num, __u32 ind, __u32 offset, __u32 block_size){
+    if (ind == 0)
+        return;
+
+    __u32 n_refs = block_size / sizeof(__u32);
+    __u32 refs[n_refs];
+
+    if (pread(img_fd, refs, block_size, inode_num * block_size) < 0) {
+        fprintf(stderr, "Error: pread failed");
+        exit(2);
+    }
+
+    for (__u32 i = 0; i < n_refs; i++){
+        if (refs[i] == 0)
+            continue;
+        fprintf(stdout, "%s,%u,%u,%u,%u,%u\n",
+        "INDIRECT", owner_inode_num,
+        ind, offset + i,
+        inode_num, refs[i]);
+
+        summarize_indirect_ref(owner_inode_num, refs[i], ind - 1, offset, block_size);
+    }
 }
 
 /*
@@ -197,9 +270,19 @@ void summarize_inode(struct ext2_super_block* super_block, struct ext2_group_des
             inode->i_links_count, ctime,
             mtime, atime,
             inode->i_size, inode->i_blocks);
-        for (int b = 0 ; b < EXT2_N_BLOCKS; b++)
+        for (int b = 0; b < EXT2_N_BLOCKS; b++)
             fprintf(stdout, ",%u", inode->i_block[b]);
         fprintf(stdout, "\n");
+
+        if (file_type == 'd')
+            summarize_directory_entry(inode, i + 1, block_size);
+
+        if (inode->i_block[12] != 0)
+            summarize_indirect_ref(i + 1, inode->i_block[12], 1, 12, block_size);
+        if (inode->i_block[13] != 0)
+            summarize_indirect_ref(i + 1, inode->i_block[13], 2, 12 + 256, block_size);
+        if (inode->i_block[14] != 0)
+            summarize_indirect_ref(i + 1, inode->i_block[14], 3, 12 + 256 + 256 * 256, block_size);
 
         free(inode);
     }
